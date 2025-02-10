e16372b3c6ae9503bc28eeeedb8209388d61f86a
yeeuniii <anna862700@gmail.com>
2025-02-10 19:51:22 +0900

    feat: 인터셉터의 redirect 제거 및 로직 수정 (#461)

--- src/main/java/com/exchangediary/global/config/web/interceptor/GroupAuthorizationInterceptor.java
+++ src/main/java/com/exchangediary/global/config/web/interceptor/GroupAuthorizationInterceptor.java
@@ -10,7 +10,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.HandlerMapping;
 
-import java.io.IOException;
 import java.util.Map;
 import java.util.Optional;
 
@@ -23,56 +22,26 @@ public class GroupAuthorizationInterceptor implements HandlerInterceptor {
             HttpServletRequest request,
             HttpServletResponse response,
             Object handler
-    ) throws IOException {
-        Map<String, String> pathVariables = (Map<String, String>) request.getAttribute(HandlerMapping.URI_TEMPLATE_VARIABLES_ATTRIBUTE);
-        Long memberId = (Long) request.getAttribute("memberId");
-        Optional<String> memberGroupId = memberQueryService.findGroupBelongTo(memberId);
-
-        handleNotExistRequestUrl(pathVariables, request.getRequestURI());
-        if (isGroupCreatePageRequest(pathVariables)) {
-            return processGroupCreatePageRequest(memberGroupId.orElse(null), response);
-        }
-
-        String groupId = String.valueOf(pathVariables.get("groupId"));
-        if (memberGroupId.isEmpty()) {
-            response.sendRedirect("/groups");
-            return false;
-        }
-        return processGroupAuthorization(groupId, memberGroupId.get(), request);
-    }
-
-    private void handleNotExistRequestUrl(Map<String, String> pathVariables, String url) {
-        if (pathVariables == null) {
-            throw new NotFoundException(ErrorCode.NOT_FOUND, "", url);
-        }
-    }
-
-    private boolean isGroupCreatePageRequest(Map<String, String> pathVariables) {
-        return !pathVariables.containsKey("groupId");
-    }
-
-    private boolean processGroupCreatePageRequest(String memberGroupId, HttpServletResponse response) throws IOException {
-        if (memberGroupId == null) {
-            return true;
-        }
-        response.sendRedirect("/groups/" + memberGroupId);
-        return false;
-    }
-
-    private boolean processGroupAuthorization(
-            String groupId,
-            String memberGroupId,
-            HttpServletRequest request
     ) {
-        if (!groupId.equals(memberGroupId)) {
-            throw new ForbiddenException(
-                    ErrorCode.GROUP_FORBIDDEN,
-                    "",
-                    request.getRequestURI()
-            );
-        }
+        Map<String, String> pathVariables = (Map<String, String>) request.getAttribute(HandlerMapping.URI_TEMPLATE_VARIABLES_ATTRIBUTE);
+        String groupIdInUri = extractGroupId(pathVariables, request.getRequestURI());
 
-        request.setAttribute("groupId", groupId);
+        Long memberId = (Long) request.getAttribute("memberId");
+        Optional<String> memberGroupId = memberQueryService.findGroupIdBelongTo(memberId);
+
+        if (memberGroupId.isEmpty() || !memberGroupId.get().equals(groupIdInUri)) {
+            throw new ForbiddenException(ErrorCode.GROUP_FORBIDDEN, "", groupIdInUri);
+        }
         return true;
+    }
+
+    private String extractGroupId(Map<String, String> pathVariables, String uri) {
+        if (pathVariables == null) {
+            throw new NotFoundException(ErrorCode.NOT_FOUND, "URI에 path variables 없음", uri);
+        }
+        if (!pathVariables.containsKey("groupId")) {
+            throw new NotFoundException(ErrorCode.NOT_FOUND, "URI에 그룹 id 포함되어 있지 않음", uri);
+        }
+        return String.valueOf(pathVariables.get("groupId"));
     }
 }
