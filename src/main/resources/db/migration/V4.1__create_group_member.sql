14d3a5a878e6c70b74afc95ed710cc2d61b48b55
yeeuniii <anna862700@gmail.com>
2025-02-20 01:10:18 +0900

    feat: 외래키에 on delete 제약조건 추가 (#464)

--- src/main/resources/db/migration/V4.1__create_group_member.sql
+++ src/main/resources/db/migration/V4.1__create_group_member.sql
@@ -10,8 +10,8 @@ CREATE TABLE IF NOT EXISTS group_member
     group_role VARCHAR(255) CHECK (group_role IN ('GROUP_LEADER', 'GROUP_MEMBER')) NOT NULL,
     member_id BIGINT NOT NULL,
     group_id VARCHAR(8) NOT NULL,
-    CONSTRAINT group_member_member_id_fkey FOREIGN KEY (member_id) REFERENCES "member" (id),
-    CONSTRAINT group_member_group_id_fkey FOREIGN KEY (group_id) REFERENCES "group" (id),
+    CONSTRAINT group_member_member_id_fkey FOREIGN KEY (member_id) REFERENCES "member" (id) ON DELETE CASCADE,
+    CONSTRAINT group_member_group_id_fkey FOREIGN KEY (group_id) REFERENCES "group" (id) ON DELETE CASCADE,
     CONSTRAINT group_member_order_in_group_check CHECK (order_in_group >= 0 AND order_in_group <= 7),
     CONSTRAINT group_member_profile_image_check CHECK (profile_image IN ('red', 'orange', 'yellow', 'green', 'blue', 'navy', 'purple'))
 );
@@ -27,16 +27,16 @@ ALTER TABLE member
     DROP COLUMN group_id;
 
 ALTER TABLE diary ADD COLUMN group_member_id BIGINT NOT NULL;
-ALTER TABLE diary ADD CONSTRAINT diary_group_member_id_fkey FOREIGN KEY (group_member_id) REFERENCES group_member(id);
+ALTER TABLE diary ADD CONSTRAINT diary_group_member_id_fkey FOREIGN KEY (group_member_id) REFERENCES group_member(id) ON DELETE CASCADE,;
 UPDATE diary SET group_member_id = member_id;
 ALTER TABLE diary DROP COLUMN member_id;
 
 ALTER TABLE comment ADD COLUMN group_member_id BIGINT NOT NULL;
-ALTER TABLE comment ADD CONSTRAINT comment_group_member_id_fkey FOREIGN KEY (group_member_id) REFERENCES group_member(id);
+ALTER TABLE comment ADD CONSTRAINT comment_group_member_id_fkey FOREIGN KEY (group_member_id) REFERENCES group_member(id) ON DELETE CASCADE,;
 UPDATE comment SET group_member_id = member_id;
 ALTER TABLE comment DROP COLUMN member_id;
 
 ALTER TABLE reply ADD COLUMN group_member_id BIGINT NOT NULL;
-ALTER TABLE reply ADD CONSTRAINT reply_group_member_id_fkey FOREIGN KEY (group_member_id) REFERENCES group_member(id);
+ALTER TABLE reply ADD CONSTRAINT reply_group_member_id_fkey FOREIGN KEY (group_member_id) REFERENCES group_member(id) ON DELETE CASCADE,;
 UPDATE reply SET group_member_id = member_id;
 ALTER TABLE reply DROP COLUMN member_id;
