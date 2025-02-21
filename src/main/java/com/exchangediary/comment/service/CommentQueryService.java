7521f2b7a5ce8590109c3992abc47df16259633a
yeeuniii <anna862700@gmail.com>
2025-02-22 03:11:48 +0900

    feat: 그룹원 엔티티 추가에 따른 댓글/답글 로직 수정 (#464)

--- src/main/java/com/exchangediary/comment/service/CommentQueryService.java
+++ src/main/java/com/exchangediary/comment/service/CommentQueryService.java
@@ -9,7 +9,8 @@ import com.exchangediary.diary.service.DiaryQueryService;
 import com.exchangediary.comment.ui.dto.response.CommentResponse;
 import com.exchangediary.global.exception.ErrorCode;
 import com.exchangediary.global.exception.serviceexception.NotFoundException;
-import com.exchangediary.member.domain.entity.Member;
+import com.exchangediary.group.domain.entity.GroupMember;
+import com.exchangediary.group.service.GroupMemberQueryService;
 import com.exchangediary.member.service.MemberQueryService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -24,31 +25,32 @@ public class CommentQueryService {
     private final DiaryQueryService diaryQueryService;
     private final DiaryAuthorizationService diaryAuthorizationService;
     private final CommentAuthorizationService commentAuthorizationService;
+    private final GroupMemberQueryService groupMemberQueryService;
 
     public Comment findComment(Long commentId) {
         return commentRepository.findById(commentId)
-                .orElseThrow(() -> new NotFoundException(
+                    .orElseThrow(() -> new NotFoundException(
                         ErrorCode.COMMENT_NOT_FOUND,
                         "",
                         String.valueOf(commentId)
                 ));
     }
 
-    public CommentCreationVerifyResponse verifyCommentCreation(Long diaryId, Long memberId) {
-        Member member = memberQueryService.findMember(memberId);
+    public CommentResponse viewComment(Long diaryId, Long memberId, Long commentId) {
+        GroupMember groupMember = groupMemberQueryService.findGroupMemberByMemberId(memberId);
         Diary diary = diaryQueryService.findDiary(diaryId);
 
-        diaryAuthorizationService.checkDiaryViewable(member, diary);
-        commentAuthorizationService.checkCommentWritable(member, diary);
-        return CommentCreationVerifyResponse.from(member.getProfileImage());
+        diaryAuthorizationService.checkDiaryViewable(groupMember.getLastViewableDiaryDate(), diary);
+        Comment comment = findComment(commentId);
+        return CommentResponse.of(comment, groupMember.getProfileImage());
     }
 
-    public CommentResponse viewComment(Long diaryId, Long memberId, Long commentId) {
-        Member member = memberQueryService.findMember(memberId);
+    public CommentCreationVerifyResponse verifyCommentCreation(Long diaryId, Long memberId) {
+        GroupMember groupMember = groupMemberQueryService.findGroupMemberByMemberId(memberId);
         Diary diary = diaryQueryService.findDiary(diaryId);
 
-        diaryAuthorizationService.checkDiaryViewable(member, diary);
-        Comment comment = findComment(commentId);
-        return CommentResponse.of(comment, member.getProfileImage());
+        diaryAuthorizationService.checkDiaryViewable(groupMember.getLastViewableDiaryDate(), diary);
+        commentAuthorizationService.checkCommentWritable(groupMember, diary);
+        return CommentCreationVerifyResponse.from(groupMember.getProfileImage());
     }
 }
--- src/main/java/com/exchangediary/comment/ui/ApiCommentController.java
+++ src/main/java/com/exchangediary/comment/ui/ApiCommentController.java
@@ -39,7 +39,7 @@ public class ApiCommentController {
             @RequestAttribute Long memberId
     ) {
         CommentCreateResponse response = commentCreateService.createComment(request, diaryId, memberId);
-        notificationService.pushNotification(diaryQueryService.findDiary(diaryId).getMember().getId(), "친구가 내 일기에 댓글을 달았어요!");
+        notificationService.pushNotification(diaryQueryService.findDiary(diaryId).getGroupMember().getId(), "친구가 내 일기에 댓글을 달았어요!");
         return ResponseEntity
                 .status(HttpStatus.CREATED)
                 .body(response);
@@ -76,8 +76,8 @@ public class ApiCommentController {
             @RequestAttribute Long memberId
     ) {
         replyCreateService.createReply(request, diaryId, commentId, memberId);
-        notificationService.pushNotification(commentQueryService.findComment(commentId).getMember().getId(), "친구가 내 댓글에 답글을 달았어요!");
-        notificationService.pushNotification(diaryQueryService.findDiary(diaryId).getMember().getId(), "친구가 내 일기의 댓글에 답글을 달았어요!");
+        notificationService.pushNotification(commentQueryService.findComment(commentId).getGroupMember().getId(), "친구가 내 댓글에 답글을 달았어요!");
+        notificationService.pushNotification(diaryQueryService.findDiary(diaryId).getGroupMember().getId(), "친구가 내 일기의 댓글에 답글을 달았어요!");
         return ResponseEntity
                 .status(HttpStatus.CREATED)
                 .build();
