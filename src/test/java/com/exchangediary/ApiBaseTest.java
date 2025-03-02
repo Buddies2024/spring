09c873c8d6d3c3c0758142cfeca9dbc65c12e040
yeeuniii <anna862700@gmail.com>
2025-03-02 15:36:56 +0900

    test: 반복되는 데이터 준비 작업 ApiBaseTest 클래스 메서드로 생성 (#464) - 그룹 생성 및 그룹 가입

--- src/test/java/com/exchangediary/ApiBaseTest.java
+++ src/test/java/com/exchangediary/ApiBaseTest.java
@@ -1,9 +1,15 @@
 package com.exchangediary;
 
+import com.exchangediary.group.domain.GroupMemberRepository;
+import com.exchangediary.group.domain.GroupRepository;
+import com.exchangediary.group.domain.entity.Group;
+import com.exchangediary.group.domain.entity.GroupMember;
+import com.exchangediary.group.domain.enums.GroupRole;
 import com.exchangediary.member.domain.MemberRepository;
 import com.exchangediary.member.domain.entity.Member;
 import com.exchangediary.member.service.JwtService;
 import io.restassured.RestAssured;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -15,36 +21,54 @@ import static org.springframework.test.context.jdbc.Sql.ExecutionPhase.BEFORE_TE
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @Sql(scripts = {"classpath:truncate.sql"}, executionPhase = BEFORE_TEST_METHOD)
 public class ApiBaseTest {
+    public static final String GROUP_NAME = "버디즈";
+    public static final String[] PROFILE_IMAGES = {"red", "orange", "yellow", "green", "blue", "navy", "purple"};
+
     @LocalServerPort
     private int port;
     @Autowired
+    protected JwtService jwtService;
+    @Autowired
     protected MemberRepository memberRepository;
     @Autowired
-    private JwtService jwtService;
+    protected GroupRepository groupRepository;
+    @Autowired
+    protected GroupMemberRepository groupMemberRepository;
 
+    protected final Long kakaoId = 1L;
     protected Member member;
     protected String token;
 
     @BeforeEach
-    void setUp() {
+    public void setUp() {
         RestAssured.port = port;
-        init();
-    }
-
-    protected void init() {
         this.member = createMember();
-        memberRepository.save(this.member);
         this.token = getToken();
     }
 
     private Member createMember() {
-        return Member.builder()
-                .kakaoId(1L)
-                .orderInGroup(0)
-                .build();
+        Member member = Member.from(kakaoId);
+        return memberRepository.save(member);
     }
 
     private String getToken() {
         return jwtService.generateAccessToken(member.getId());
+    }
+
+    protected Group createGroup() {
+        Group group = Group.from(GROUP_NAME);
+        return groupRepository.save(group);
+    }
+
+    protected GroupMember joinGroup(String nickname, int profileImageIndex, int orderInGroup, GroupRole groupRole, Group group, Member member) {
+        GroupMember groupMember = GroupMember.of(
+                nickname,
+                PROFILE_IMAGES[profileImageIndex],
+                orderInGroup,
+                groupRole,
+                group,
+                member
+        );
+        return groupMemberRepository.save(groupMember);
     }
 }
