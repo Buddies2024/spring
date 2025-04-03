072f7cf207c06ff7e7517f3392ec496cfc0ca7a3
yeeuniii <anna862700@gmail.com>
2025-04-03 15:36:52 +0900

    test: 그룹원 정보 조회 API 테스트 수정 (#464)

--- src/test/java/com/exchangediary/group/api/GroupMembersOrderApiTest.java
+++ src/test/java/com/exchangediary/group/api/GroupMembersOrderApiTest.java
@@ -1,191 +1,82 @@
 package com.exchangediary.group.api;
 
 import com.exchangediary.ApiBaseTest;
-import com.exchangediary.diary.domain.DiaryRepository;
-import com.exchangediary.diary.domain.entity.Diary;
-import com.exchangediary.group.domain.GroupRepository;
 import com.exchangediary.group.domain.entity.Group;
+import com.exchangediary.group.domain.enums.GroupRole;
 import com.exchangediary.group.ui.dto.response.GroupMembersResponse;
-import com.exchangediary.member.domain.MemberRepository;
-import com.exchangediary.member.domain.entity.Member;
-import com.exchangediary.member.domain.enums.GroupRole;
 import io.restassured.RestAssured;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class GroupMembersOrderApiTest extends ApiBaseTest {
-    private static final List<String> PROFILE_IMAGES = new ArrayList<>(List.of("red", "orange", "yellow", "green", "blue", "navy", "purple"));
-    @Autowired
-    private GroupRepository groupRepository;
-    @Autowired
-    private MemberRepository memberRepository;
-    @Autowired
-    private DiaryRepository diaryRepository;
+    private static final String URI = "/api/groups/%s/members";
 
     @Test
-    void 그룹원_순서_조회_내순서_1번일때() {
+    @DisplayName("사용자는 3번이고, 리더는 1번, 현재 일기 작성자는 5번인 경우")
+    void Expect_SelfIs2AndLeaderIs0AndWriterIs4() {
+        // Given
         Group group = createGroup();
-        this.member.joinGroup("self", PROFILE_IMAGES.get(0), 1, GroupRole.GROUP_LEADER, group);
-        memberRepository.save(member);
-        for (int idx = 6; idx > 0; idx--) {
-            createMember(group, idx + 1, PROFILE_IMAGES.get(7 - idx));
+
+        joinGroup("리더", 1, 1, GroupRole.GROUP_LEADER, group, createMember(1234L));
+        for (int idx = 1 ; idx < 7; idx++) {
+            if (idx == 2) {
+                joinGroup("스프링", idx, idx + 1, GroupRole.GROUP_MEMBER, group, this.member);
+            } else {
+                joinGroup("그룹원" + idx, idx, idx + 1, GroupRole.GROUP_MEMBER, group, createMember(idx * 10L));
+            }
         }
-
-        var response = RestAssured
-                .given().log().all()
-                .cookie("token", token)
-                .when()
-                .get("/api/groups/" + group.getId() + "/members")
-                .then().log().all()
-                .statusCode(HttpStatus.OK.value())
-                .extract().as(GroupMembersResponse.class);
-        var members = response.members();
-
-        assertThat(members).hasSize(7);
-        assertThat(members.get(0).nickname()).isEqualTo(this.member.getNickname());
-        assertThat(members.get(1).nickname()).isEqualTo("name2");
-        assertThat(members.get(2).nickname()).isEqualTo("name3");
-        assertThat(members.get(3).nickname()).isEqualTo("name4");
-        assertThat(members.get(4).nickname()).isEqualTo("name5");
-        assertThat(members.get(5).nickname()).isEqualTo("name6");
-        assertThat(members.get(6).nickname()).isEqualTo("name7");
-        assertThat(response.selfIndex()).isEqualTo(0);
-        assertThat(response.leaderIndex()).isEqualTo(0);
-        assertThat(response.currentWriterIndex()).isEqualTo(0);
-    }
-
-    @Test
-    void 그룹원_순서_조회_내순서_4번일때() {
-        Group group = createGroup();
-        this.member.joinGroup("self", PROFILE_IMAGES.get(0), 4, GroupRole.GROUP_LEADER, group);
-        memberRepository.save(member);
-        for (int idx = 3; idx > 0; idx--) {
-            createMember(group, idx, PROFILE_IMAGES.get(7 - idx));
-        }
-        for (int idx = 7; idx > 4; idx--) {
-            createMember(group, idx, PROFILE_IMAGES.get(7 - idx));
-        }
-
-        var response = RestAssured
-                .given().log().all()
-                .cookie("token", token)
-                .when()
-                .get("/api/groups/" + group.getId() + "/members")
-                .then().log().all()
-                .statusCode(HttpStatus.OK.value())
-                .extract().as(GroupMembersResponse.class);
-        var members = response.members();
-
-        assertThat(members).hasSize(7);
-        assertThat(members.get(0).nickname()).isEqualTo("name1");
-        assertThat(members.get(1).nickname()).isEqualTo("name2");
-        assertThat(members.get(2).nickname()).isEqualTo("name3");
-        assertThat(members.get(3).nickname()).isEqualTo(this.member.getNickname());
-        assertThat(members.get(4).nickname()).isEqualTo("name5");
-        assertThat(members.get(5).nickname()).isEqualTo("name6");
-        assertThat(members.get(6).nickname()).isEqualTo("name7");
-        assertThat(response.selfIndex()).isEqualTo(3);
-        assertThat(response.leaderIndex()).isEqualTo(3);
-        assertThat(response.currentWriterIndex()).isEqualTo(0);
-    }
-
-    @Test
-    void 그룹원_순서_조회_내순서_7번일때() {
-        Group group = createGroup();
-        this.member.joinGroup("self", PROFILE_IMAGES.get(0), 7, GroupRole.GROUP_LEADER, group);
-        memberRepository.save(member);
-        for (int idx = 6; idx > 0; idx--) {
-            createMember(group, idx, PROFILE_IMAGES.get(7 - idx));
-        }
-
-        var response = RestAssured
-                .given().log().all()
-                .cookie("token", token)
-                .when()
-                .get("/api/groups/" + group.getId() + "/members")
-                .then().log().all()
-                .statusCode(HttpStatus.OK.value())
-                .extract().as(GroupMembersResponse.class);
-        var members = response.members();
-
-        assertThat(members).hasSize(7);
-        assertThat(members.get(0).nickname()).isEqualTo("name1");
-        assertThat(members.get(1).nickname()).isEqualTo("name2");
-        assertThat(members.get(2).nickname()).isEqualTo("name3");
-        assertThat(members.get(3).nickname()).isEqualTo("name4");
-        assertThat(members.get(4).nickname()).isEqualTo("name5");
-        assertThat(members.get(5).nickname()).isEqualTo("name6");
-        assertThat(members.get(6).nickname()).isEqualTo(this.member.getNickname());
-        assertThat(response.selfIndex()).isEqualTo(6);
-        assertThat(response.leaderIndex()).isEqualTo(6);
-        assertThat(response.currentWriterIndex()).isEqualTo(0);
-    }
-
-    @Test
-    @DisplayName("current writer이 오늘 일기 작성자 반환")
-    void 그룹원_순서_조회_오늘_일기_작성() {
-        Group group = createGroup();
-        this.member.joinGroup("self", PROFILE_IMAGES.get(0), 1, GroupRole.GROUP_LEADER, group);
-        memberRepository.save(member);
-        for (int idx = 6; idx > 0; idx--) {
-            createMember(group, idx + 1, PROFILE_IMAGES.get(7 - idx));
-        }
-        createDiary(group);
-        group.updateCurrentOrder(group.getCurrentOrder() + 1, 7);
+        group.changeCurrentOrder(5);
         groupRepository.save(group);
 
-        var response = RestAssured
+        // When
+        GroupMembersResponse response = RestAssured
                 .given().log().all()
                 .cookie("token", token)
-                .when()
-                .get("/api/groups/" + group.getId() + "/members")
+                .when().get(String.format(URI, group.getId()))
                 .then().log().all()
                 .statusCode(HttpStatus.OK.value())
                 .extract().as(GroupMembersResponse.class);
-        var members = response.members();
+
+        // Then
+        List<GroupMembersResponse.GroupMemberResponse> members = response.members();
 
         assertThat(members).hasSize(7);
-        assertThat(members.get(0).nickname()).isEqualTo(this.member.getNickname());
-        assertThat(members.get(1).nickname()).isEqualTo("name2");
-        assertThat(members.get(2).nickname()).isEqualTo("name3");
-        assertThat(members.get(3).nickname()).isEqualTo("name4");
-        assertThat(members.get(4).nickname()).isEqualTo("name5");
-        assertThat(members.get(5).nickname()).isEqualTo("name6");
-        assertThat(members.get(6).nickname()).isEqualTo("name7");
+
+        assertThat(members.get(0).nickname()).isEqualTo("리더");
+        assertThat(members.get(1).nickname()).isEqualTo("그룹원1");
+        assertThat(members.get(2).nickname()).isEqualTo("스프링");
+        assertThat(response.selfIndex()).isEqualTo(2);
+        assertThat(response.leaderIndex()).isEqualTo(0);
+        assertThat(response.currentWriterIndex()).isEqualTo(4);
+    }
+
+    @Test
+    @DisplayName("그룹원이 한 명인 경우")
+    void When_MemberIsSolo_Expect_AllIndexIs0() {
+        Group group = createGroup();
+
+        joinGroup("스프링", 1, 1, GroupRole.GROUP_LEADER, group, this.member);
+
+        GroupMembersResponse response = RestAssured
+                .given().log().all()
+                .cookie("token", token)
+                .when().get(String.format(URI, group.getId()))
+                .then().log().all()
+                .statusCode(HttpStatus.OK.value())
+                .extract().as(GroupMembersResponse.class);
+
+        List<GroupMembersResponse.GroupMemberResponse> members = response.members();
+
+        assertThat(members).hasSize(1);
+
+        assertThat(members.get(0).nickname()).isEqualTo("스프링");
         assertThat(response.selfIndex()).isEqualTo(0);
         assertThat(response.leaderIndex()).isEqualTo(0);
-        assertThat(response.currentWriterIndex()).isEqualTo(1);
-    }
-
-    private Group createGroup() {
-        return groupRepository.save(Group.from("GROUP_NAME"));
-    }
-
-    private void createMember(Group group, int index, String profileImage) {
-        Member member = Member.builder()
-                .profileImage(profileImage)
-                .kakaoId(1234L + index)
-                .orderInGroup(index)
-                .nickname("name" + index)
-                .groupRole(GroupRole.GROUP_MEMBER)
-                .group(group)
-                .build();
-        memberRepository.save(member);
-    }
-
-    private void createDiary(Group group) {
-        Diary diary = Diary.builder()
-                .todayMood("sleepy.svg")
-                .group(group)
-                .member(member)
-                .build();
-        diaryRepository.save(diary);
+        assertThat(response.currentWriterIndex()).isEqualTo(0);
     }
 }
