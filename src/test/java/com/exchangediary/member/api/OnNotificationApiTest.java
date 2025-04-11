757a3314168d7b6d0bc2793d1b955dd1a2d5503b
yeeuniii <anna862700@gmail.com>
2025-04-12 01:57:29 +0900

    test: 사용자의 알림 활성화 여부 조회 API 테스트 작성 (#491)

---
+++ src/test/java/com/exchangediary/member/api/OnNotificationApiTest.java
@@ -0,0 +1,28 @@
+package com.exchangediary.member.api;
+
+import com.exchangediary.ApiBaseTest;
+import com.exchangediary.member.ui.dto.response.MemberNotificationResponse;
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpStatus;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class OnNotificationApiTest extends ApiBaseTest {
+    private static final String URI = "/api/member/notification";
+
+    @Test
+    @DisplayName("사용자의 알림 활성화 여부를 조회한다.")
+    void When_MembersOnNotificationIsTrue() {
+        MemberNotificationResponse body = RestAssured
+                .given().log().all()
+                .cookie("token", token)
+                .when().get(URI)
+                .then().log().all()
+                .statusCode(HttpStatus.OK.value())
+                .extract().as(MemberNotificationResponse.class);
+
+        assertThat(body.onNotification()).isTrue();
+    }
+}
