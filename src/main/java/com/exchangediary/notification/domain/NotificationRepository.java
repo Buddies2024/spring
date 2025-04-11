c5ecbe95b1c97c40cde7a4704011edaeaac5a3b9
yeeuniii <anna862700@gmail.com>
2025-04-12 03:19:49 +0900

    feat: 알림 활성화되어있는 사용자의 토큰만 가져오도록 sql문 수정 (#491)

--- src/main/java/com/exchangediary/notification/domain/NotificationRepository.java
+++ src/main/java/com/exchangediary/notification/domain/NotificationRepository.java
@@ -9,8 +9,14 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 public interface NotificationRepository extends JpaRepository<Notification, Long> {
-    List<Notification> findAllByMemberId(Long memberId);
     boolean existsByToken(String token);
+
+    @Query("""
+        SELECT n.token
+        FROM Notification n
+        WHERE n.member.onNotification = true
+    """)
+    List<String> findTokensByMemberId(Long memberId);
 
     @Query("""
         SELECT n.token
@@ -20,6 +26,7 @@ public interface NotificationRepository extends JpaRepository<Notification, Long
             ON gm.member = m
         WHERE gm.group.id = :groupId
             AND m.id <> :memberId
+            AND m.onNotification = true
     """)
     List<String> findTokensByGroupIdAndExcludeMemberId(String groupId, Long memberId);
 
@@ -32,6 +39,7 @@ public interface NotificationRepository extends JpaRepository<Notification, Long
         WHERE gm.group.id = :groupId
             AND m.id <> :memberId
             AND gm.groupRole <> 'GROUP_LEADER'
+            AND m.onNotification = true
     """)
     List<String> findTokensByGroupIdAndExcludeMemberIdAndLeader(String groupId, Long memberId);
 
@@ -43,6 +51,7 @@ public interface NotificationRepository extends JpaRepository<Notification, Long
             ON gm.member = m
         WHERE gm.group.id = :groupId
             AND gm.orderInGroup = gm.group.currentOrder
+            AND m.onNotification = true
     """)
     List<String> findTokensByGroupIdAndCurrentOrder(String groupId);
 
@@ -57,6 +66,7 @@ public interface NotificationRepository extends JpaRepository<Notification, Long
             AND CAST(d.createdAt AS DATE) = CURRENT_DATE
         WHERE gm.orderInGroup = gm.group.currentOrder
             AND d.id IS NULL
+            AND m.onNotification = true
     """)
     List<String> findTokensByMembersWithoutDiaryToday();
 
